69bee66d1d7dbd1e8f3509d45dcdf7c707429290
truongvantho1 <157790637+truongvantho1@users.noreply.github.com>
2024-11-14 17:56:47 +0700

    fix login đăng nhập

--- cart.php
+++ cart.php
@@ -1,3 +1,4 @@
+<?php include('controllers/auth.controller.php') ?>
 <?php include('components/header.php') ?>
 
 <div class="bg-light py-3">
@@ -95,20 +96,12 @@
                         <button class="btn btn-primary btn-sm btn-block">Update Cart</button>
                     </div>
                     <div class="col-md-6">
-                        <button class="btn btn-outline-primary btn-sm btn-block">Continue Shopping</button>
+                        <button class="btn btn-outline-primary btn-sm btn-block"><a href="shop.php">Continue
+                                Shopping</a></button>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-md-12">
-                        <label class="text-black h4" for="coupon">Coupon</label>
-                        <p>Enter your coupon code if you have one.</p>
-                    </div>
-                    <div class="col-md-8 mb-3 mb-md-0">
-                        <input type="text" class="form-control py-3" id="coupon" placeholder="Coupon Code">
-                    </div>
-                    <div class="col-md-4">
-                        <button class="btn btn-primary btn-sm">Apply Coupon</button>
-                    </div>
+
                 </div>
             </div>
             <div class="col-md-6 pl-5">
